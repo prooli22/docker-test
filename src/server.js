779e7556104b816ad57a5d22c14430edcb5ebf71
Olivier Provost <olivier.provost22@gmail.com>
2019-05-06 21:43:39 -0400

    wifi info + setup received

--- src/server.js
+++ src/server.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const wifi = require('node-wifi');
+const app = express();
 
+// APP SETUP
+app.use(express.json());
+
+// CORS SETUP
 cors({
     origin: '*',
 });
-
 app.use(cors());
 app.options('*', cors());
 
+// WIFI SETUP
+wifi.init({ iface: null });
+
 app.get('/', (req, res) => {
-    //res.json('YOU\'VE TEST NODE API SERVER');
-    return res.send('Received GET HTTP method');
+    return res.send('NEURHOME Hub is here.');
+});
+
+app.get('/wifi', (req, res) => {
+    wifi.scan((err, network) => {
+        if (err) console.log(err);
+        else {
+            network = network.map(e => e.ssid)
+                             .map((e, i, final) => final.indexOf(e) === i && i)
+                             .filter(e => network[e])
+                             .map(e => network[e]);
+            network.sort((a, b) => b.signal_level - a.signal_level);
+            return res.send(network);
+        }
+    })
+});
+
+app.post('/setup', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
 });
     
 app.listen(5000, () => {
